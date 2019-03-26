0d01e456f5c414f5d69f0ebe3e000eb6366d765f
Tirathangil <tirathangil@yandex.ru>
2019-03-26 16:55:57 +0300

    Probing other solutions for parser

--- sidelibs/maildbparser.cpp
+++ sidelibs/maildbparser.cpp
@@ -3,66 +3,90 @@
 void ReadDBFile(ifstream &ReadStream)
 {
     string ParsingLine;
+    bool Folder(false),Label(false),Message(false);
+
     ReadStream.open("messages.db");
     if(!ReadStream.is_open())
     {
         return;
     }
+
     while(ReadStream.eof()!=true)
     {
-        int Paired(0);                      //Переменная для подсчета треугольных скобок. < = +1, > = -1;
-        getline(ReadStream,ParsingLine);
-        string::iterator ParsingSymbol = ParsingLine.begin();
-        string Tag,Parameter,Value;
-        while(ParsingSymbol != ParsingLine.end())
+        string::iterator ParsingSymbol;
+        int FindSym;
+        string Tag,Parameter,Value,TagText;
+
+        getline(ReadStream,ParsingLine,'<');
+        getline(ReadStream,ParsingLine,'>');
+
+        ParsingSymbol=ParsingLine.begin;
+        FindSym=ParsingLine.find(' ');
+        Tag=ParsingLine.substr(0,ParsingLine.find(' '));
+
+        while()
         {
-            if(*ParsingSymbol == ' ')
-            {
-                ParsingSymbol++;
-            }
-            if(*ParsingSymbol == '<')
-            {
-                Paired++;
-                while(*ParsingSymbol != ' ')
-                {
-                    ParsingSymbol++;
-                    Tag.push_back(*ParsingSymbol);
-                }
-                GetTagParameter(Tag);
-                ParsingSymbol++;
-            }
-            if(*ParsingSymbol == '>')
-            {
-                Paired--;
-                ParsingSymbol++;
-                continue;
-            }
 
-            if(Paired != 0)
-            {
-                string TagParameters;
-                while(*ParsingSymbol==' ')
-                    ParsingSymbol++;
-                while(*ParsingSymbol!=' ')
-                {
-                    TagParameters.push_back(*ParsingSymbol);
-                    ParsingSymbol++;
-                }
 
-                string::iterator ParseValues = TagParameters.begin();
-                while(*ParseValues!='=')
-                {
-                    Parameter.push_back(*ParseValues);
-                    ParseValues++;
-                }
-                ParseValues++;
-                while (ParseValues!=TagParameters.end())
-                {
-                    Value.push_back(*ParseValues);
-                    ParseValues++;
-                }
-            }
         }
+
+//        getline(ReadStream,ParsingLine);
+
+//        while(ParsingSymbol != ParsingLine.end())
+//        {
+//            if(*ParsingSymbol == ' ')
+//            {
+//                ParsingSymbol++;
+//            }
+//            if(*ParsingSymbol == '<')
+//            {
+//                Paired++;
+//                while(*ParsingSymbol != ' ')
+//                {
+//                    ParsingSymbol++;
+//                    Tag.push_back(*ParsingSymbol);
+//                }
+//                GetTagParameter(Tag);
+//                ParsingSymbol++;
+//            }
+//
+//            while(Paired != 0)
+//            {
+//                string TagParameters;
+//                while(*ParsingSymbol==' ')
+//                    ParsingSymbol++;
+//                while(*ParsingSymbol!=' ')
+//                {
+//                    TagParameters.push_back(*ParsingSymbol);
+//                    ParsingSymbol++;
+//                }
+
+//                string::iterator ParseValues = TagParameters.begin();
+//                while(*ParseValues!='=')
+//                {
+//                    Parameter.push_back(*ParseValues);
+//                    ParseValues++;
+//                }
+//                ParseValues++;
+//                while (ParseValues!=TagParameters.end())
+//                {
+//                    if(*ParsingSymbol == '>')
+//                    {
+//                        Paired--;
+//                        ParsingSymbol++;
+//                    }
+//                    Value.push_back(*ParseValues);
+//                    ParseValues++;
+//                }
+
+//                if(*ParsingSymbol == '>')
+//                {
+//                    Paired--;
+//                    ParsingSymbol++;
+//                }
+//            }
+
+//        }
     }
     ReadStream.close();
 }
